90be3047315900a87214c3831adc6d407e0b1156
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2023-07-10 09:46:09 -0400

    Basic Form and Dummy Data Mapped

--- lib/screens/grocery_list_screen.dart
+++ lib/screens/grocery_list_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/screens/new_item_screen.dart';
 
-class GroceryListScreen extends StatelessWidget {
+class GroceryListScreen extends StatefulWidget {
   const GroceryListScreen({super.key});
+
+  @override
+  State<GroceryListScreen> createState() {
+    return _GroceryListScreenState();
+  }
+}
+
+class _GroceryListScreenState extends State<GroceryListScreen> {
+
+  void _addItem() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const NewItemScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+              onPressed: _addItem,
+              icon: Icon(Icons.add))
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
